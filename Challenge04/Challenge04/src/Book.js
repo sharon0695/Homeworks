ce0c6a8abf2d6da554cb875671af6a5ab84a800d
sharon0695 <shzuray@gmail.com>
2026-03-11 16:02:31 -0500

    Realizo challenge04 stacks

---
+++ Challenge04/Challenge04/src/Book.js
@@ -0,0 +1,10 @@
+class Book {
+    constructor(name, ISBN, author, editorial){
+        this.name = name;
+        this.ISBN = ISBN;
+        this.author = author;
+        this.editorial = editorial;
+    }
+}
+
+export default Book;
